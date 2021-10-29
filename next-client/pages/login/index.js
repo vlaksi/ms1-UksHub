27a89b66689beee0ed67401b02c8a48d3b6a2e8b
Vladislav Maksimovic <daksrapsody@gmail.com>
2021-10-29 18:54:42 +0200

    chore: check deploy on vercel

--- next-client/pages/login/index.js
+++ next-client/pages/login/index.js
@@ -6,7 +6,7 @@ const Login = () => {
 			<Col md={4}>
 				<Form className="mt-5">
 					<Form.Group className="mb-3" controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
+						<Form.Label>Email address*</Form.Label>
 						<Form.Control type="email" placeholder="Enter email" />
 						<Form.Text className="text-muted">
 							We'll never share your email with anyone else.
@@ -14,7 +14,7 @@ const Login = () => {
 					</Form.Group>
 
 					<Form.Group className="mb-3" controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
+						<Form.Label>Password*</Form.Label>
 						<Form.Control type="password" placeholder="Password" />
 					</Form.Group>
 
