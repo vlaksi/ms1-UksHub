af432c64a407c0662570a28f41fe97590571e385
Ana Perisic <anaperisictb@yahoo.com>
2021-10-29 21:52:41 +0200

    feature[fe]: changing branch on code tab

--- next-client/common/components/molecules/Repository/Code/RepositoryCode.js
+++ next-client/common/components/molecules/Repository/Code/RepositoryCode.js
@@ -1,7 +1,54 @@
+import { useState } from 'react';
+import { Card, Button, Dropdown } from 'react-bootstrap';
+
+const branches = [
+	{
+		branchName: 'main',
+	},
+	{
+		branchName: 'develop',
+	},
+	{
+		branchName: 'feature/yyy',
+	},
+];
+
 const RepositoryCode = () => {
+	const [branchName, setBranchName] = useState(branches[0].branchName);
+
 	return (
 		<>
-			<h3> hi from RepositoryCode</h3>
+			<Card>
+				<Card.Header>
+					<Dropdown>
+						<Dropdown.Toggle variant="dark" id="dropdown-basic">
+							{branchName}
+						</Dropdown.Toggle>
+
+						<Dropdown.Menu>
+							{branches.map((branch) => {
+								return (
+									<Dropdown.Item
+										onClick={() => {
+											setBranchName(branch.branchName);
+										}}
+									>
+										{branch.branchName}
+									</Dropdown.Item>
+								);
+							})}
+						</Dropdown.Menu>
+					</Dropdown>
+				</Card.Header>
+				<Card.Body>
+					<Card.Title>Special title treatment</Card.Title>
+					<Card.Text>
+						With supporting text below as a natural lead-in to additional
+						content.
+					</Card.Text>
+				</Card.Body>
+				<Card.Footer className="text-muted">2 days ago</Card.Footer>
+			</Card>
 		</>
 	);
 };
