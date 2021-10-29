7da58dd811c547935c186f867fdc18e0bf6ebf6c
Vladislav Maksimovic <daksrapsody@gmail.com>
2021-10-29 19:43:39 +0200

    feature[fe]: layout customized

--- next-client/common/components/molecules/Layout/Layout.js
+++ next-client/common/components/molecules/Layout/Layout.js
@@ -1,13 +1,14 @@
+import styles from './Layout.module.scss';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
 const Layout = ({ children }) => {
 	return (
-		<>
+		<div className={styles.layoutWrapper}>
 			<Navbar />
 			<div>{children}</div>
 			<Footer />
-		</>
+		</div>
 	);
 };
 
