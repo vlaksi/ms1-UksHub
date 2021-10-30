69b2159bfdd6eaad2d41e21eb798e39040c5e3f1
Ana Perisic <anaperisictb@yahoo.com>
2021-10-30 13:26:12 +0200

    feature[fe]: alignment on content of repository & path to file

--- next-client/common/components/molecules/Repository/Code/RepositoryCode.js
+++ next-client/common/components/molecules/Repository/Code/RepositoryCode.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Card, Dropdown, ListGroup } from 'react-bootstrap';
+import { Card, Dropdown, ListGroup, Row } from 'react-bootstrap';
 import { BsFillFolderFill } from 'react-icons/bs';
 import { AiOutlineFile } from 'react-icons/ai';
+import styles from './RepositoryCode.module.scss';
 
 const backendFolder = {
 	name: 'django-backend',
@@ -58,39 +59,50 @@ const branches = [
 	},
 ];
 
-// 3. Enable click on folder which
-
 const RepositoryCode = () => {
 	const [activeBranch, setActiveBranch] = useState(branches[0]);
 	const [activeFolders, setActiveFolders] = useState(branches[0].folders);
 	const [activeFiles, setActiveFiles] = useState(branches[0].files);
+	const [activeFilesPath, setActiveFilesPath] = useState([]);
 
 	return (
 		<>
 			<Card>
 				<Card.Header>
-					<Dropdown>
-						<Dropdown.Toggle variant="dark" id="dropdown-basic">
-							{activeBranch.branchName}
-						</Dropdown.Toggle>
+					<div className={styles.repositoryHeader}>
+						<Dropdown>
+							<Dropdown.Toggle variant="dark" id="dropdown-basic">
+								{activeBranch.branchName}
+							</Dropdown.Toggle>
 
-						<Dropdown.Menu>
-							{branches.map((branch) => {
+							<Dropdown.Menu>
+								{branches.map((branch) => {
+									return (
+										<Dropdown.Item
+											key={branch.branchName}
+											onClick={() => {
+												setActiveBranch(branch);
+												setActiveFolders(branch.folders);
+												setActiveFiles(branch.files);
+												setActiveFilesPath([]);
+											}}
+										>
+											{branch.branchName}
+										</Dropdown.Item>
+									);
+								})}
+							</Dropdown.Menu>
+						</Dropdown>
+						<div className={styles.filesPath}>
+							{activeFilesPath.map((activePath, idx) => {
 								return (
-									<Dropdown.Item
-										key={branch.branchName}
-										onClick={() => {
-											setActiveBranch(branch);
-											setActiveFolders(branch.folders);
-											setActiveFiles(branch.files);
-										}}
-									>
-										{branch.branchName}
-									</Dropdown.Item>
+									<p key={activePath}>
+										{idx === 0 ? '' : '/'} {activePath}
+									</p>
 								);
 							})}
-						</Dropdown.Menu>
-					</Dropdown>
+						</div>
+					</div>
 				</Card.Header>
 				<Card.Body>
 					<ListGroup>
@@ -101,9 +113,14 @@ const RepositoryCode = () => {
 									key={folder.name}
 									onClick={() => {
 										setActiveFolders(folder.folders);
+										setActiveFiles(folder.files);
+										setActiveFilesPath([...activeFilesPath, folder.name]);
 									}}
 								>
-									<BsFillFolderFill /> {folder.name}
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<BsFillFolderFill style={{ marginRight: '8px' }} />
+										{folder.name}
+									</div>
 								</ListGroup.Item>
 							);
 						})}
@@ -113,16 +130,20 @@ const RepositoryCode = () => {
 									action
 									key={file.name}
 									onClick={() => {
-										setActiveFolders(file.folders);
+										alert(file.name);
 									}}
 								>
-									<AiOutlineFile /> {file.name}
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<AiOutlineFile style={{ marginRight: '8px' }} /> {file.name}
+									</div>
 								</ListGroup.Item>
 							);
 						})}
 					</ListGroup>
 				</Card.Body>
-				<Card.Footer className="text-muted">2 days ago</Card.Footer>
+				<Card.Footer className="text-muted">
+					<div className={styles.repositoryFooter}>2 days ago</div>
+				</Card.Footer>
 			</Card>
 		</>
 	);
