9656e1f786ff896f09b168e5380c25655e930318
Nemanja Pualic <nemanja.pualic@jsguru.io>
2021-11-03 21:22:54 +0100

    feature[backend]: project conected to postgres sql db

--- django-backend/ukshub/ukshub/settings.py
+++ django-backend/ukshub/ukshub/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'ukshub.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'uks',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
