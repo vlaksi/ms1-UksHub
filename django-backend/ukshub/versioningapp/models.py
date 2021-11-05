8e43822d26dab933237a7c108d0158594d80f235
Pufke <pualic.nemanja@gmail.com>
2021-11-05 16:18:06 +0100

    feature[backend]: new version of model for versioningapp

--- django-backend/ukshub/versioningapp/models.py
+++ django-backend/ukshub/versioningapp/models.py
@@ -6,11 +6,11 @@ class File(models.Model):
     creation_date= models.DateTimeField('date of creation')
     def __str__(self):
         return 'Name of object: ' + self.name
-        
+
 class Folder(models.Model):
-    parent_directory = models.ForeignKey('self', on_delete=models.CASCADE, related_name='parent_folder', blank=True)
-    sub_directories = models.ManyToManyField('self', blank=True)
-    files = models.ManyToManyField(File, blank=True)
+    parent_directory = models.ForeignKey('self', on_delete=models.CASCADE, related_name='parent_folder', blank=True, null=True)
+    sub_directories = models.ManyToManyField('self',blank=True, null=True)
+    files = models.ManyToManyField(File, blank=True, null=True)
     name = models.CharField(max_length=200)
     last_change = models.DateTimeField('last change')
     def __str__(self):
@@ -21,20 +21,20 @@ class Commit(models.Model):
     message = models.CharField(max_length=200)
     creation_date = models.DateTimeField('date of creation')
     def __str__(self):
-        return 'Name of object: ' + self.name
+        return 'Name of object: ' + self.message
 
 class Branch(models.Model):
-    child_branchs = models.ManyToManyField('self', blank=True)
-    parent_branch = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
-    files = models.ManyToManyField(File, blank=True)
-    folders = models.ManyToManyField(Folder, blank=True)
+    child_branchs = models.ManyToManyField('self', blank=True, null=True)
+    parent_branch = models.ForeignKey('self', on_delete=models.CASCADE,blank=True, null=True)
+    files = models.ManyToManyField(File,blank=True, null=True)
+    folders = models.ManyToManyField(Folder, blank=True, null=True)
     name = models.CharField(max_length=200)
-    commits = models.ManyToManyField(Commit, blank=True)
+    commits = models.ManyToManyField(Commit, blank=True, null=True)
     def __str__(self):
         return 'Name of object: ' + self.name
 
 class Repository(models.Model):
-    branches = models.ManyToManyField(Branch, blank=True)
+    branches = models.ManyToManyField(Branch, blank=True, null=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     def __str__(self):
