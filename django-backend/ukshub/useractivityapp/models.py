5e9f429444ccedc8e0f3cce1c5e1014cb25379b5
Pufke <pualic.nemanja@gmail.com>
2021-11-05 17:37:27 +0100

    feature[backend]: iniital version of useractivityapp model

--- django-backend/ukshub/useractivityapp/models.py
+++ django-backend/ukshub/useractivityapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class ActionType(models.Model):  
+    name = models.CharField(max_length=200)
+    path_to_icon = models.CharField(max_length=200)
+    def __str__(self):
+        return 'Name of object: ' + self.name
+
+
+class ReactionType(models.Model):  
+    name= models.CharField(max_length=200)
+    path_to_icon= models.CharField(max_length=200)
+    def __str__(self):
+        return 'Name of object: ' + self.name
+
+class Action(models.Model):  
+    type =  models.ForeignKey(to=ActionType, null=False, on_delete=models.CASCADE)
+class Reaction(models.Model):  
+    type =  models.ForeignKey(to=ReactionType, null=False, on_delete=models.CASCADE)
+
+class Comment(models.Model):  
+    message = models.CharField(max_length=200)
+    creation_date = models.DateTimeField('date of creation')
 # Create your models here.
--- django-backend/ukshub/versioningapp/models.py
+++ django-backend/ukshub/versioningapp/models.py
@@ -9,8 +9,8 @@ class File(models.Model):
 
 class Folder(models.Model):
     parent_directory = models.ForeignKey('self', on_delete=models.CASCADE, related_name='parent_folder', blank=True, null=True)
-    sub_directories = models.ManyToManyField('self',blank=True, null=True)
-    files = models.ManyToManyField(File, blank=True, null=True)
+    sub_directories = models.ManyToManyField('self',blank=True)
+    files = models.ManyToManyField(File, blank=True)
     name = models.CharField(max_length=200)
     last_change = models.DateTimeField('last change')
     def __str__(self):
@@ -24,17 +24,17 @@ class Commit(models.Model):
         return 'Name of object: ' + self.message
 
 class Branch(models.Model):
-    child_branchs = models.ManyToManyField('self', blank=True, null=True)
+    child_branchs = models.ManyToManyField('self', blank=True)
     parent_branch = models.ForeignKey('self', on_delete=models.CASCADE,blank=True, null=True)
-    files = models.ManyToManyField(File,blank=True, null=True)
-    folders = models.ManyToManyField(Folder, blank=True, null=True)
+    files = models.ManyToManyField(File,blank=True)
+    folders = models.ManyToManyField(Folder, blank=True)
     name = models.CharField(max_length=200)
-    commits = models.ManyToManyField(Commit, blank=True, null=True)
+    commits = models.ManyToManyField(Commit, blank=True)
     def __str__(self):
         return 'Name of object: ' + self.name
 
 class Repository(models.Model):
-    branches = models.ManyToManyField(Branch, blank=True, null=True)
+    branches = models.ManyToManyField(Branch, blank=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     def __str__(self):
