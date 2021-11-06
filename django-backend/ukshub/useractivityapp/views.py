860854822911be19304f890c42c3659461d35938
Pufke <pualic.nemanja@gmail.com>
2021-11-06 23:46:10 +0100

    feature[bakend]: initial authentication on bakend

--- django-backend/ukshub/useractivityapp/views.py
+++ django-backend/ukshub/useractivityapp/views.py
@@ -1,13 +1,15 @@
 from django.db.models import query
 from django.shortcuts import render
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, permissions
 from .models import Comment
 from .serializers import CommentSerializer
 
 class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CommentSerializer
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CommentSerializer
