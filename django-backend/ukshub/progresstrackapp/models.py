74edd7e0da29b763ae723cb950a66f46add8dd12
Pufke <pualic.nemanja@gmail.com>
2021-11-06 21:30:31 +0100

    feature[backend]: initial model for progresstrack app

--- django-backend/ukshub/progresstrackapp/models.py
+++ django-backend/ukshub/progresstrackapp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from versioningapp.models import Branch
+
+class Label(models.Model):  
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    decription = models.CharField(max_length=200)
+    def __str__(self):
+         return 'Name of object: ' + self.name 
+class Issue(models.Model):  
+    title= models.CharField(max_length=200)
+    creation_date= models.DateTimeField('date of creation')
+    is_opened = models.BooleanField(default=False)
+    labels = models.ManyToManyField(Label, blank=True)
+    def __str__(self):
+        return 'Title of object: ' + self.title
+class Milestone(models.Model):  
+    title = models.CharField(max_length=200)
+    due_date = models.DateTimeField('due date')
+    description = models.CharField(max_length=200)
+    issues = models.ManyToManyField(Issue, blank=True)
+    def __str__(self):
+        return 'Title of object: ' + self.title       
+          
+class PullRequest(models.Model):  
+    base_branch =  models.ForeignKey(to=Branch, null=False, on_delete=models.CASCADE,related_name='base_branchs')
+    compare_branch =  models.ForeignKey(to=Branch, null=False, on_delete=models.CASCADE,related_name='compare_branchs')
+    is_able_to_merge = models.BooleanField(default=False)
+    is_merged = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+    creation_date = models.DateTimeField('creation date')
+    title = models.CharField(max_length=200)
+    issues = models.ManyToManyField(Issue, blank=True)
+    milestones = models.ManyToManyField(Milestone, blank=True)
+    labels = models.ManyToManyField(Label, blank=True)
+    def __str__(self):
+        return 'Title of object: ' + self.title    
 
 # Create your models here.
--- django-backend/ukshub/useractivityapp/models.py
+++ django-backend/ukshub/useractivityapp/models.py
@@ -6,7 +6,6 @@ class ActionType(models.Model):
     def __str__(self):
         return 'Name of object: ' + self.name
 
-
 class ReactionType(models.Model):  
     name= models.CharField(max_length=200)
     path_to_icon= models.CharField(max_length=200)
@@ -15,9 +14,12 @@ class ReactionType(models.Model):
 
 class Action(models.Model):  
     type =  models.ForeignKey(to=ActionType, null=False, on_delete=models.CASCADE)
+    def __str__(self):
+        return 'Type of Action: ' + self.type.name
 class Reaction(models.Model):  
     type =  models.ForeignKey(to=ReactionType, null=False, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return 'Type of Reaction: ' + self.type.name
 class Comment(models.Model):  
     message = models.CharField(max_length=200)
     creation_date = models.DateTimeField('date of creation')
