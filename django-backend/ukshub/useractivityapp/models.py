c64efe438a33044b98e3fcf2b0ae7bde3e09faa9
Pufke <pualic.nemanja@gmail.com>
2021-11-07 15:19:54 +0100

    feature[backend]: all API endpoints for useractivityapp

--- django-backend/ukshub/useractivityapp/models.py
+++ django-backend/ukshub/useractivityapp/models.py
@@ -19,12 +19,14 @@ class Action(models.Model):
     def __str__(self):
         return 'Type of Action: ' + self.type.name
 class Reaction(models.Model):  
-    reaction = models.ForeignKey(to=User, null=False, on_delete=models.CASCADE,related_name='reaction_created') 
+    author = models.ForeignKey(to=User, null=False, on_delete=models.CASCADE,related_name='reactions_created') 
     type =  models.ForeignKey(to=ReactionType, null=False, on_delete=models.CASCADE)
     def __str__(self):
         return 'Type of Reaction: ' + self.type.name
+        
 class Comment(models.Model):  
     author = models.ForeignKey(to=User, null=False, on_delete=models.CASCADE,related_name='comment_created') 
+    reaction = models.ManyToManyField(to=Reaction, blank=True, related_name='comment') 
     message = models.CharField(max_length=200)
     creation_date = models.DateTimeField('date of creation')
 
--- django-backend/ukshub/useractivityapp/serializers.py
+++ django-backend/ukshub/useractivityapp/serializers.py
@@ -1,12 +1,31 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import Comment
+from .models import Action, Comment, Reaction, ReactionType
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = [ "pk", "message", "creation_date", "author" ]
-        #if we don't want some field to be Required
-        # extra_kwargs = {
-        #      "creation_date": {"required": False},
-        # }
+        fields = [ "pk", "message", "creation_date", "author" , "reaction"]
+        extra_kwargs = {
+             "reaction": {"required": False},
+        }
+
+class ReactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reaction
+        fields = [ "pk", "author", "type" ]
+
+class ActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Action
+        fields = [ "pk", "author", "type" ]
+
+class ReactionTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReactionType
+        fields = [ "pk", "name", "path_to_icon" ]
+
+class ActionTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReactionType
+        fields = [ "pk", "name", "path_to_icon" ]
