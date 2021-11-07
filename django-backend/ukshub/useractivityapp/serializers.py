320ed866ca9643e138ce032cb44ed6b66843dbab
Pufke <pualic.nemanja@gmail.com>
2021-11-07 14:53:58 +0100

    fix[backend]: swager for delete patch and put

--- django-backend/ukshub/useractivityapp/serializers.py
+++ django-backend/ukshub/useractivityapp/serializers.py
@@ -5,7 +5,8 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = [ "pk", "message", "creation_date" ]
+        fields = [ "pk", "message", "creation_date", "author" ]
+        #if we don't want some field to be Required
         # extra_kwargs = {
         #      "creation_date": {"required": False},
         # }
