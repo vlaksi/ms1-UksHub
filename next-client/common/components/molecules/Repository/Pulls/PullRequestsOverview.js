55c18f1a73ec0e92f55668aa31112b20395869e2
Ana Perisic <anaperisictb@yahoo.com>
2021-11-10 21:50:57 +0100

    feature[fe][pull request crud]: added buttons for labels,milestons and new pr

--- next-client/common/components/molecules/Repository/Pulls/PullRequestsOverview.js
+++ next-client/common/components/molecules/Repository/Pulls/PullRequestsOverview.js
@@ -1,7 +1,16 @@
+import { Button} from 'react-bootstrap';
+import { BsFillTagsFill } from "react-icons/bs";
+import { GoMilestone } from "react-icons/go";
+import { MdAddCircle } from "react-icons/md";
+
 const PullRequestsOverview = () => {
 	return (
 		<>
-			<h3> hi from PullRequestsOverview</h3>
+			<div style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<Button style={{marginRight: '5px'}} variant="primary" variant="outline-primary"><BsFillTagsFill size={18}/> Labels</Button>
+				<Button style={{marginRight: '5px', marginLeft: '5px'}} variant="primary" variant="outline-primary"><GoMilestone size={18}/> Milestones</Button>
+				<Button style={{marginLeft: '5px'}} variant="primary"><MdAddCircle size={24}/> Add pull request</Button>
+			</div>
 		</>
 	);
 };
