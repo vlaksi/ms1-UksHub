9671c5e586c37d122498e3180b2f02717ab5b78a
Ana Perisic <anaperisictb@yahoo.com>
2021-11-10 20:47:49 +0100

    feature[fe][crud repository]: added modal for creating new repository

--- next-client/common/components/molecules/User/Repositories/UserRepositories.js
+++ next-client/common/components/molecules/User/Repositories/UserRepositories.js
@@ -1,6 +1,7 @@
 import RepositoryListItem from '../../../atoms/RepositoryListItem/RepositoryListItem';
-import { Button } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
 import { MdAddCircle } from "react-icons/md";
+import {useState} from 'react';
 
 // TODO: Get this repositories for passed username
 const repositories = [
@@ -10,15 +11,49 @@ const repositories = [
 	{
 		name: 'Election-DAPP',
 	},
+
 ];
 
 const UserRepositories = ({ username }) => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
 	return (
 		<>
 
 			<div style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<Button variant="outline-primary" >	<MdAddCircle size={24}/> Add repository</Button>
+				<Button variant="outline-primary" onClick={handleShow}>	<MdAddCircle size={24}/> Add repository</Button>
+
+				<Modal show={show} onHide={handleClose} backdrop="static">
+					<Modal.Header closeButton>
+						<Modal.Title>Add new repository</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<Form>
+							<Form.Group className="mb-3">
+								<Form.Label>Name of repository</Form.Label>
+								<Form.Control type="name" placeholder="Enter name of repository" />
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>Description of repository</Form.Label>
+								<Form.Control as="textarea" rows={3} placeholder="Describe your repository"/>
+							</Form.Group>
+						</Form>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant="danger" onClick={handleClose}>
+							Cancel
+						</Button>
+						<Button variant="success" onClick={handleClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
+
+
 			{repositories.map((repository) => {
 				return (
 					<RepositoryListItem
