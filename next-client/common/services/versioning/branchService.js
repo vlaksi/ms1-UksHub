749152f323c55c3d9e7c6f8d17d066ba8259618a
Ana Perisic <anaperisictb@yahoo.com>
2021-11-17 21:14:21 +0100

    refactor[fe][crud repository]: changed username and password of the superuser

--- next-client/common/services/versioning/branchService.js
+++ next-client/common/services/versioning/branchService.js
@@ -1,51 +1,51 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getDefaultBranch = async (branchId) => {
-  let branch;
-  await axios
-    .request({
-      url: `/versioning/branchs/${branchId}`,
-      method: "get",
-      baseURL: "http://127.0.0.1:8000/",
-      auth: {
-        username: "anci", // This is the client_id
-        password: "root", // This is the client_secret
-      },
-      data: {
-        grant_type: "client_credentials",
-        scope: "public",
-      },
-    })
-    .then((response) => {
-      branch = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branch;
+	let branch;
+	await axios
+		.request({
+			url: `/versioning/branchs/${branchId}`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			auth: {
+				username: 'admin', // This is the client_id
+				password: 'root', // This is the client_secret
+			},
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branch = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branch;
 };
 
 export const getRepositoryBranches = async (repositoryId) => {
-  let branches;
-  await axios
-    .request({
-      url: `/versioning/repository/${repositoryId}/branches/`,
-      method: "get",
-      baseURL: "http://127.0.0.1:8000/",
-      auth: {
-        username: "anci", // This is the client_id
-        password: "root", // This is the client_secret
-      },
-      data: {
-        grant_type: "client_credentials",
-        scope: "public",
-      },
-    })
-    .then((response) => {
-      branches = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branches;
+	let branches;
+	await axios
+		.request({
+			url: `/versioning/repository/${repositoryId}/branches/`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			auth: {
+				username: 'admin', // This is the client_id
+				password: 'root', // This is the client_secret
+			},
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branches = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branches;
 };
