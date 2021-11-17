943673c2c2457500d6f33506d7f0954e7db5d31a
Ana Perisic <anaperisictb@yahoo.com>
2021-11-17 18:23:15 +0100

    fix[fe][pr]: key for the pr list

--- next-client/common/components/atoms/PullRequestList/PullRequestList.js
+++ next-client/common/components/atoms/PullRequestList/PullRequestList.js
@@ -1,30 +1,24 @@
 import { Card, Button } from 'react-bootstrap';
-import {MdArrowForwardIos} from "react-icons/md";
+import { MdArrowForwardIos } from 'react-icons/md';
 
-  const PullRequestList = ({pullRequests}) => {
-    const renderCards = (card) => {
-		return(
-			<div>
-				<Card key={card.title} border="primary" style={{ width: '40rem', marginTop:'15px' }} >
+const PullRequestList = ({ pullRequests }) => {
+	const renderCards = (card) => {
+		return (
+			<div key={card.title}>
+				<Card border="primary" style={{ width: '40rem', marginTop: '15px' }}>
 					<Card.Body>
 						<Card.Title>{card.title}</Card.Title>
-						<Card.Text>
-							#36 Opened 12/10/2021 by vaksi {card.text}
-
-						</Card.Text>	
-						<Button variant="primary" style={{marginLeft:'25rem'}}>Show details <MdArrowForwardIos /> </Button>
+						<Card.Text>#36 Opened 12/10/2021 by vaksi {card.text}</Card.Text>
+						<Button variant="primary" style={{ marginLeft: '25rem' }}>
+							Show details <MdArrowForwardIos />{' '}
+						</Button>
 					</Card.Body>
 				</Card>
 			</div>
 		);
-	  };
+	};
 
+	return <>{pullRequests.map(renderCards)}</>;
+};
 
-    return(
-        <>
-            {pullRequests.map(renderCards)}
-        </>
-    );
-  };
-
-  export default PullRequestList;
+export default PullRequestList;
