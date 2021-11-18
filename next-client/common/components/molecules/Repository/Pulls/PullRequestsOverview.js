1ddeba499e208757b928a2352084a15a5eb1213c
Ana Perisic <anaperisictb@yahoo.com>
2021-11-18 20:45:31 +0100

    feature[fe][pull request]: fetched branches to compare branch option

--- next-client/common/components/molecules/Repository/Pulls/PullRequestsOverview.js
+++ next-client/common/components/molecules/Repository/Pulls/PullRequestsOverview.js
@@ -129,10 +129,9 @@ const PullRequestsOverview = ({ dbRepository }) => {
                     handleBaseBranchAdding(e.target.value);
                   }}
                 >
-                  <option>Open this select menu</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                  {allBranches?.map((branch) => {
+                    return <option>{branch.name}</option>;
+                  })}
                 </Form.Select>
               </Form.Group>
             </Form>
