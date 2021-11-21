ac033c5aa966cb75ee4e25bb914b223642dbcd20
Ana Perisic <anaperisictb@yahoo.com>
2021-11-21 21:44:22 +0100

    refactor[fe][pull request]: design of the date rendering and show details btn

--- next-client/common/components/atoms/PullRequestList/PullRequestList.js
+++ next-client/common/components/atoms/PullRequestList/PullRequestList.js
@@ -12,29 +12,44 @@ const PullRequestList = ({ pullRequests }) => {
       <div key={pullRequest.pk}>
         <Card border="primary" style={{ width: "40rem", marginTop: "15px" }}>
           <Card.Body>
-            <Card.Title>
-              <Badge pill bg="primary" text="light">
-                #{pullRequest.pk}
-              </Badge>{" "}
-              {pullRequest.title}
+            <Card.Title
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div style={{ display: "flex" }}>
+                <Badge
+                  pill
+                  bg="primary"
+                  text="light"
+                  style={{ marginRight: "10px" }}
+                >
+                  #{pullRequest.pk}
+                </Badge>{" "}
+                {pullRequest.title}
+              </div>
+              <div>
+                <Button variant="primary">
+                  <Link href={`/${user}/${repository}/pulls/${pullRequest.pk}`}>
+                    <a style={{ textDecoration: "none", color: "white" }}>
+                      Show details <MdArrowForwardIos />{" "}
+                    </a>
+                  </Link>
+                </Button>
+              </div>
             </Card.Title>
             <Card.Text>
               Opened
               <Badge pill bg="light" text="dark">
-                {pullRequest.creation_date}
+                {pullRequest.creation_date.substring(0, 10)}
               </Badge>
               by{" "}
               <Badge pill bg="light" text="dark">
                 {user}
               </Badge>
             </Card.Text>
-            <Button variant="primary" style={{ marginLeft: "25rem" }}>
-              <Link href={`/${user}/${repository}/pulls/${pullRequest.pk}`}>
-                <a style={{ textDecoration: "none", color: "white" }}>
-                  Show details <MdArrowForwardIos />{" "}
-                </a>
-              </Link>
-            </Button>
           </Card.Body>
         </Card>
       </div>
