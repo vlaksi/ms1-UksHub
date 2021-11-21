4f63b18d015a666a2f9ec4e261321e827da2f0c0
Ana Perisic <anaperisictb@yahoo.com>
2021-11-21 18:16:37 +0100

    feature[fe][pull request]: added button and modal for deleting pr

--- next-client/common/components/organisms/PullRequests/PullRequestDetails.js
+++ next-client/common/components/organisms/PullRequests/PullRequestDetails.js
@@ -1,8 +1,68 @@
+import { Card, Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+
 const PullRequestDetails = ({ pullRequestId }) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const handleDeleteModalClose = () => setShowDeleteModal(false);
+  const handleShowDeleteModal = () => setShowDeleteModal(true);
+
   return (
     <>
-      <h1>Napolju pada veliki sneg!!!!</h1>
+      <h1>Add style for this page</h1>
       <p> PullRequestIndex: {pullRequestId}</p>
+      <Card border="danger" style={{ width: "50%" }}>
+        <Card.Header>Danger Zone</Card.Header>
+        <Card.Body
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Card.Title>Delete this pull request</Card.Title>
+          <Button variant="danger" onClick={handleShowDeleteModal}>
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+
+      {/* Delete pull request */}
+      <Modal show={showDeleteModal} onHide={handleDeleteModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: " baseline",
+          }}
+        >
+          <p>Are you sure you want to delete this pull request ?</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            variant="success"
+            onClick={async () => {
+              //await deleteChosenRepository();
+              handleDeleteModalClose();
+              // TODO: change this link
+              //window.location.href = `http://localhost:3000/${user}`;
+            }}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="danger"
+            onClick={() => {
+              handleDeleteModalClose();
+            }}
+          >
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
