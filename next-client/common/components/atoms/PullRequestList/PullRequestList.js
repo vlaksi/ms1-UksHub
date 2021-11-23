5472a49ebd2ab66346e71318330fdcd7385f01a7
Ana Perisic <anaperisictb@yahoo.com>
2021-11-23 18:46:06 +0100

    refactor[fe][pull request]: extracted pull request item component

--- next-client/common/components/atoms/PullRequestList/PullRequestList.js
+++ next-client/common/components/atoms/PullRequestList/PullRequestList.js
@@ -1,69 +1,17 @@
-import { Card, Button, Badge } from "react-bootstrap";
-import { MdArrowForwardIos } from "react-icons/md";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { getUserById } from "../../../services/useractivity/userService";
+import PullRequestItem from '../PullRequestItem/PullRequestItem';
 
 const PullRequestList = ({ pullRequests }) => {
-  const router = useRouter();
-  const { user, repository } = router.query;
-
-  const renderCards = (pullRequest) => {
-    const getUser = async () => {
-      let user = await getUserById(pullRequest.author);
-      let retValue;
-      retValue = user.username;
-      console.log(pullRequest.author);
-      console.log(retValue);
-    };
-
-    return (
-      <div key={pullRequest.pk}>
-        <Card border="primary" style={{ width: "40rem", marginTop: "15px" }}>
-          <Card.Body>
-            <Card.Title
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <div style={{ display: "flex" }}>
-                <Badge
-                  pill
-                  bg="primary"
-                  text="light"
-                  style={{ marginRight: "10px" }}
-                >
-                  #{pullRequest.pk}
-                </Badge>{" "}
-                {pullRequest.title}
-              </div>
-              <div>
-                <Button variant="primary">
-                  <Link href={`/${user}/${repository}/pulls/${pullRequest.pk}`}>
-                    <a style={{ textDecoration: "none", color: "white" }}>
-                      Show details <MdArrowForwardIos />{" "}
-                    </a>
-                  </Link>
-                </Button>
-              </div>
-            </Card.Title>
-            <Card.Text>
-              Opened
-              <Badge pill bg="light" text="dark">
-                {pullRequest.creation_date.substring(0, 10)}
-              </Badge>
-              by
-              <Badge pill bg="light" text="dark"></Badge>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  };
-
-  return <>{pullRequests.map(renderCards)}</>;
+	return (
+		<>
+			{pullRequests.map((pullRequest) => {
+				return (
+					<div key={pullRequest.pk}>
+						<PullRequestItem pullRequest={pullRequest} />
+					</div>
+				);
+			})}
+		</>
+	);
 };
 
 export default PullRequestList;
