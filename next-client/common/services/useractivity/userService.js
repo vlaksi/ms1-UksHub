aab38fb29508f363a3fcacdee38252f94740a6bc
Vladislav Maksimovic <daksrapsody@gmail.com>
2021-11-24 22:26:09 +0100

    fix[fe][actions]: deleted duplicated code

--- next-client/common/services/useractivity/userService.js
+++ next-client/common/services/useractivity/userService.js
@@ -1,68 +1,43 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const getAllUsers = async () => {
-  let users;
-  await axios
-    .request({
-      url: `/useractivity/users/`,
-      method: "get",
-      baseURL: "http://127.0.0.1:8000/",
-      auth: {
-        username: "admin", // This is the client_id
-        password: "root", // This is the client_secret
-      },
-      data: {
-        grant_type: "client_credentials",
-        scope: "public",
-      },
-    })
-    .then((response) => {
-      users = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return users;
+	let users;
+	await axios
+		.request({
+			url: `/useractivity/users/`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			auth: {
+				username: 'admin', // This is the client_id
+				password: 'root', // This is the client_secret
+			},
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			users = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return users;
 };
 
 export const getUserDataForSearch = async () => {
-  const users = await getAllUsers();
+	const users = await getAllUsers();
 
-  let dataForSearch = [];
-  users.map((user) => {
-    dataForSearch.push({
-      title: user.username,
-      pk: user.pk,
-      type: "user",
-    });
-  });
+	let dataForSearch = [];
+	users.map((user) => {
+		dataForSearch.push({
+			title: user.username,
+			pk: user.pk,
+			type: 'user',
+		});
+	});
 
-  return dataForSearch;
-};
-
-export const getUserById = async (authorId) => {
-  let user;
-  await axios
-    .request({
-      url: `/useractivity/users/${authorId}`,
-      method: "get",
-      baseURL: "http://127.0.0.1:8000/",
-      auth: {
-        username: "admin", // This is the client_id
-        password: "root", // This is the client_secret
-      },
-      data: {
-        grant_type: "client_credentials",
-        scope: "public",
-      },
-    })
-    .then((response) => {
-      user = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return user;
+	return dataForSearch;
 };
 
 export const getUserById = async (authorId) => {
