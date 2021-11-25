e82d6166c33acda24c9efdb543e370f62a4e60bb
Pufke <pualic.nemanja@gmail.com>
2021-11-25 02:20:41 +0100

    feature[authentication]: registration endpoint fixed

--- django-backend/ukshub/authentication/models.py
+++ django-backend/ukshub/authentication/models.py
@@ -5,20 +5,35 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
             raise ValueError('Users must have an email address')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-
+        user = self.model( username=username, **extra_fields)
+    
         user.set_password(password)
+        user.is_active = True
         user.save()
 
+        return user
+    def create_superuser(self, username, email, password,**extra_fields):
+        """
+         Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+             email=email,
+             username=username,
+             password=password,**extra_fields
+        )
+        user.staff = True
+        user.admin = True
+        user.is_active = True
+        user.save(using=self._db)
         return user
 
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255,unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -26,8 +41,8 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     objects = UserAccountManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = 'username'
+ 
 
     def get_full_name(self):
         return self.first_name
