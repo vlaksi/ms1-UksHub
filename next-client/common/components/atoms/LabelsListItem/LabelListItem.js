7f569b9a26c02cf7ade0afc14608284ef8f9dd03
Ana Perisic <anaperisictb@yahoo.com>
2021-12-08 17:09:37 +0100

    feature[fe][labels]: added buttons for editing and deleting labels

--- next-client/common/components/atoms/LabelsListItem/LabelListItem.js
+++ next-client/common/components/atoms/LabelsListItem/LabelListItem.js
@@ -1,4 +1,5 @@
-import { ListGroup, Badge } from "react-bootstrap";
+import { ListGroup, Badge, Button } from "react-bootstrap";
+import { MdModeEditOutline, MdDelete } from "react-icons/md";
 
 const LabelListItem = ({ label }) => {
   return (
@@ -9,11 +10,31 @@ const LabelListItem = ({ label }) => {
       >
         <div className="ms-2 me-auto">
           <div className="fw-bold">
-            <Badge variant="primary" pill>
+            <Badge bg={label.color} pill>
               {label.name}
             </Badge>
           </div>
           {label.description}
+        </div>
+        <div style={{ display: "flex" }}>
+          <Button
+            variant="outline-success"
+            style={{ marginRight: "15px" }}
+            size="sm"
+          >
+            <MdModeEditOutline
+              style={{ marginBottom: "4px" }}
+            ></MdModeEditOutline>
+            Edit label
+          </Button>
+          <Button
+            variant="outline-danger"
+            size="sm"
+            style={{ marginRight: "15px" }}
+          >
+            <MdDelete style={{ marginBottom: "4px" }}></MdDelete>
+            Delete label
+          </Button>
         </div>
       </ListGroup.Item>
     </ListGroup>
--- next-client/common/components/molecules/Labels/Labels.js
+++ next-client/common/components/molecules/Labels/Labels.js
@@ -3,16 +3,22 @@ import LabelListItem from "../../atoms/LabelsListItem/LabelListItem";
 const Labels = () => {
   const labels = [
     {
+      pk: 1,
       name: "frontend",
       description: "very nice",
+      color: "primary",
     },
     {
+      pk: 2,
       name: "backend",
       description: "important label",
+      color: "danger",
     },
     {
+      pk: 3,
       name: "devops",
       description: "must do",
+      color: "success",
     },
   ];
   return (
