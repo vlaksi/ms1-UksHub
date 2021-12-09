4c928da29b4955b8323ccb849f6ba51f2aec132d
Ana Perisic <anaperisictb@yahoo.com>
2021-12-09 11:13:06 +0100

    feature[fe][labels]: added button for adding new label

--- next-client/common/components/molecules/Labels/Labels.js
+++ next-client/common/components/molecules/Labels/Labels.js
@@ -1,4 +1,6 @@
 import LabelListItem from "../../atoms/LabelsListItem/LabelListItem";
+import { Button } from "react-bootstrap";
+import { MdAddCircle } from "react-icons/md";
 
 const Labels = () => {
   const labels = [
@@ -23,6 +25,9 @@ const Labels = () => {
   ];
   return (
     <>
+      <Button style={{ marginBottom: "10px" }} variant="primary" size="sm">
+        <MdAddCircle size={18} /> Add label
+      </Button>
       {labels.map((labelItem) => {
         return (
           <div key={labelItem.pk}>
