1e07c5aba8d9ec555a1ac7c67fb911ebb398ea00
Dusan Miljkovic <dusanmiljkovic_95@hotmail.com>
2021-12-30 22:57:36 +0100

    PR changes

--- next-client/common/services/useractivity/userService.js
+++ next-client/common/services/useractivity/userService.js
@@ -1,62 +1,63 @@
 import axios from 'axios';
 
 export const getAllUsers = async () => {
-	let users;
-	await axios
-		.request({
-			url: `/useractivity/users/`,
-			method: 'get',
-			baseURL: 'http://127.0.0.1:8000/',
-			headers: {'Authorization': 'JWT '+ localStorage.getItem('token')},
-			data: {
-				grant_type: 'client_credentials',
-				scope: 'public',
-			},
-		})
-		.then((response) => {
-			users = response.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-	return users;
+    let users;
+    await axios
+        .request({
+            url: `/useractivity/users/`,
+            method: 'get',
+            baseURL: 'http://127.0.0.1:8000/',
+            headers: { Authorization: 'JWT ' + localStorage.getItem('token') },
+            data: {
+                grant_type: 'client_credentials',
+                scope: 'public',
+            },
+        })
+        .then((response) => {
+            users = response.data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    return users;
 };
 
 export const getUserDataForSearch = async () => {
-	const users = await getAllUsers();
+    const users = await getAllUsers();
 
-	let dataForSearch = [];
-	users.map((user) => {
-		dataForSearch.push({
-			title: user.username,
-			pk: user.pk,
-			type: 'user',
-		});
-	});
+    let dataForSearch = [];
+    users.map((user) => {
+        dataForSearch.push({
+            title: user.username,
+            pk: user.pk,
+            type: 'user',
+        });
+    });
 
-	return dataForSearch;
+    return dataForSearch;
 };
 
 export const getUserById = async (authorId) => {
-	let user;
-	await axios
-		.request({
-			url: `/useractivity/users/${authorId}`,
-			method: 'get',
-			baseURL: 'http://127.0.0.1:8000/',
-			headers: {'Authorization': 'JWT '+ localStorage.getItem('token')},
-			data: {
-				grant_type: 'client_credentials',
-				scope: 'public',
-			},
-		})
-		.then((response) => {
-			user = response.data;
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-	return user;
+    let user;
+    await axios
+        .request({
+            url: `/useractivity/users/${authorId}`,
+            method: 'get',
+            baseURL: 'http://127.0.0.1:8000/',
+            headers: { Authorization: 'JWT ' + localStorage.getItem('token') },
+            data: {
+                grant_type: 'client_credentials',
+                scope: 'public',
+            },
+        })
+        .then((response) => {
+            user = response.data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    return user;
+};
 
 export const addUser = async (newUserUsername, newUserPassword, newUserFirstName, newUserLastName, newUserEmail) => {
     let repository = null;
