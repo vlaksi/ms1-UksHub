422db9cc814f5686de259e835fcccb7d30e2e421
Ana Perisic <anaperisictb@yahoo.com>
2022-01-02 18:34:19 +0100

    test[pr]: added a initialize db function

--- django-backend/ukshub/progresstrackapp/tests/test_models.py
+++ django-backend/ukshub/progresstrackapp/tests/test_models.py
@@ -1,6 +1,26 @@
-from django.test import SimpleTestCase
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-class TestPullRequestModel(SimpleTestCase):
+USER1_USERNAME = 'user1'
+USER1_PASSWORD = 'Ovojejakasifra!'
+USER1_FIRST_NAME = 'Ivana'
+USER1_LAST_NAME = 'Perisic'
+USER1_EMAIL = 'ica@gmail.com'
+
+
+def initialize_db_with_test_data():
+    user1 = User.objects.create_user(username=USER1_USERNAME, password=USER1_PASSWORD,first_name=USER1_FIRST_NAME,last_name=USER1_LAST_NAME,email=USER1_EMAIL)
+    user1.save()
+
+class TestPullRequestModel(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        initialize_db_with_test_data()
+
 
     def test_title_pull_request(self):
         assert 1 == 1
