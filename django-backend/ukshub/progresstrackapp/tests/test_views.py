06d407b6e4a7aeb17f3379a4bd86110e90efeae8
Ana Perisic <anaperisictb@yahoo.com>
2022-01-04 14:22:22 +0100

    test|refactor[pull-request]: added a HTTP404 test case & adjusted endpoint to support 404 error

--- django-backend/ukshub/progresstrackapp/tests/test_views.py
+++ django-backend/ukshub/progresstrackapp/tests/test_views.py
@@ -9,9 +9,9 @@ from versioningapp.models import Branch, Repository
 def get_repo_id(repository_id=0):
     if repository_id != -1:
         repo_id = Repository.objects.all()[repository_id].id
-    else:
+    else: # return repo_id that for sure does not exist
         repositories = Repository.objects.all()
-        repo_id = repositories[len(repositories) - 1].id + 1
+        repo_id = repositories[len(repositories) - 1].id + 999
     return repo_id
 
 class TestPullRequestListView(TestCase):
@@ -20,11 +20,19 @@ class TestPullRequestListView(TestCase):
     def setUpTestData(cls):
         initialize_db_with_test_data()
 
-    def get_repository_pull_requests(self, repo_id=0):
-        repo_id = get_repo_id(repo_id)
+    def get_repository_pull_requests(self, repository_id=0):
+        repo_id = get_repo_id(repository_id)
         response = self.client.get(reverse('all-repository-pull-requests', kwargs={'repo_id': repo_id}))
         return response, repo_id
 
-    def test_view_url_access_by_name(self):
+    def test_get_all_repository_pull_requests(self):
         response, _ = self.get_repository_pull_requests()
         self.assertEqual(response.status_code, 200)
+
+    def test_get_all_repository_pull_requests(self):
+        response, _ = self.get_repository_pull_requests()
+        self.assertEqual(response.status_code, 200)
+
+    def test_HTTP404_if_pull_request_does_not_exist(self):
+        response, _ = self.get_repository_pull_requests(-1)
+        self.assertEqual(response.status_code, 404)
