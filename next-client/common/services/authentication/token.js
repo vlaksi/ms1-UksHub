6d5606e496b7a22a258be48ec80540768342bb53
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-01-06 23:34:54 +0100

    feature[manage-settings]: added token handling

---
+++ next-client/common/services/authentication/token.js
@@ -0,0 +1,26 @@
+// TODO: Change token name to smth more Domain oriented
+const TOKEN_NAME = "token"
+
+export function getToken() {
+    return localStorage.getItem(TOKEN_NAME);
+}
+
+export function saveToken(token) {    
+    localStorage.setItem(TOKEN_NAME, token);
+}
+
+export function getParsedToken() {
+    let token = localStorage.getItem(TOKEN_NAME);
+    var base64Payload = token.split(".")[1];
+    var payload = Buffer.from(base64Payload, "base64");
+    return JSON.parse(payload.toString());
+}
+
+export function removeToken() {
+    localStorage.removeItem(TOKEN_NAME);
+}
+
+export function getUsernameFromToken() {
+    let parsedToken=getParsedToken();
+    return parsedToken.sub;
+}
