f2e1c87578f5e5a2976ad1f3f2126afc650d971d
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-01-09 01:50:14 +0100

    test[settings]: finish collaboration model tests

--- django-backend/ukshub/versioningapp/tests/test_models.py
+++ django-backend/ukshub/versioningapp/tests/test_models.py
@@ -13,6 +13,12 @@ USER1_FIRST_NAME = 'Ivana'
 USER1_LAST_NAME = 'Perisic'
 USER1_EMAIL = 'ica@gmail.com'
 
+USER2_USERNAME = 'user2'
+USER2_PASSWORD = 'Ovojejakasifra!'
+USER2_FIRST_NAME = 'Jovana'
+USER2_LAST_NAME = 'Perisic'
+USER2_EMAIL = 'dragica@gmail.com'
+
 # Repository init consts
 REPOSITORY_1_NAME = "UksHub"
 REPOSITORY_2_NAME = "MatHub"
@@ -25,7 +31,10 @@ COLLABORATION_TYPE_DEVELOPER= "developer"
 def initialize_db_with_test_data():
     # Create users
     user1 = User.objects.create_user(username=USER1_USERNAME, password=USER1_PASSWORD,first_name=USER1_FIRST_NAME,last_name=USER1_LAST_NAME,email=USER1_EMAIL)
+    user2 = User.objects.create_user(username=USER2_USERNAME, password=USER2_PASSWORD,first_name=USER2_FIRST_NAME,last_name=USER2_LAST_NAME,email=USER2_EMAIL)
+    
     user1.save()
+    user2.save()
 
     # Create repositories
     repository1 = Repository.objects.create(author=user1, name=REPOSITORY_1_NAME)
@@ -41,13 +50,24 @@ def initialize_db_with_test_data():
     collaboration_type_1.save()
     collaboration_type_2.save()
 
+    # Create collaborations
+    collaboration1 = Collaboration.objects.create(collaborator=user1, repository=repository1, collaboration_type=get_collaboration_type())
+    collaboration2 = Collaboration.objects.create(collaborator=user2, repository=repository1, collaboration_type=get_collaboration_type(1))
+    collaboration3 = Collaboration.objects.create(collaborator=user2, repository=repository2, collaboration_type=get_collaboration_type())
 
+    collaboration1.save()    
+    collaboration2.save()    
+    collaboration3.save()    
 
 def get_repository(index=0):
     return Repository.objects.all()[index]
 
 def get_collaboration_type(index=0):
     return CollaborationType.objects.all()[index]
+
+def get_collaboration(index=0):
+    return Collaboration.objects.all()[index]
+
 
 class TestRepositoryModel(TestCase):
 
@@ -114,3 +134,21 @@ class TestCollaborationTypeModel(TestCase):
     def test_collaboration_type_developer_name(self):
         collaboration_type = get_collaboration_type(1)
         self.assertEqual(collaboration_type.name, COLLABORATION_TYPE_DEVELOPER)
+
+class TestCollaborationModel(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        initialize_db_with_test_data()
+
+    def test_collaboration_collaborator_username(self):
+        collaboration = get_collaboration()
+        self.assertEqual(collaboration.collaborator.username, USER1_USERNAME)
+
+    def test_collaboration_repository_name(self):
+        collaboration = get_collaboration()
+        self.assertEqual(collaboration.repository.name, REPOSITORY_1_NAME)
+
+    def test_collaboration_collaboration_type_owner(self):
+        collaboration = get_collaboration()
+        self.assertEqual(collaboration.collaboration_type.name, COLLABORATION_TYPE_OWNER)
