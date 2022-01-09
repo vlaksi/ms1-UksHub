bdda6afa73e91198074b91d5198eebf3813ee859
Pufke <pualic.nemanja@gmail.com>
2022-01-09 10:35:55 +0100

    bugfix[be][authentication]: views tests

--- django-backend/ukshub/authentication/tests/test_views.py
+++ django-backend/ukshub/authentication/tests/test_views.py
@@ -26,6 +26,18 @@ def get_jwt_token():
     response = c.post('/auth/jwt/create/', {'username': USER1_USERNAME, 'password': USER1_PASSWORD})
     return json.loads(response.content.decode('UTF-8'))['access']
 
+def get_mocked_user(test_user_firstname='Some_USER_Test'):
+    user_id = User.objects.get(username=USER1_USERNAME).pk
+
+    user = {
+        "username": USER2_USERNAME,
+        "email": USER2_EMAIL,
+        "first_name": test_user_firstname,
+        "last_name": USER2_LAST_NAME,
+        "password": USER2_PASSWORD
+    }
+    return user
+
 class TestAuthenticationViews(TestCase):
 
     @classmethod
@@ -57,3 +69,57 @@ class TestAuthenticationViews(TestCase):
         res_obj = json.loads(response.content.decode('UTF-8'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(res_obj),1)
+
+    def test_refresh_token(self):
+        response = self.c.post('/auth/jwt/refresh/', {'refresh': self.token})
+        self.assertEqual(response.status_code, 401)
+    
+    def test_jwt_token_verify(self):
+        response = self.c.post('/auth/jwt/verify/', HTTP_AUTHORIZATION=self.token, data=json.dumps({"token": self.token}), content_type=JSON)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user(self):
+        pr = User.objects.get(username=USER1_USERNAME)
+        response = self.c.get('/auth/users/'+str(pr.pk)+'/', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_put_user(self):
+        u = User.objects.get(username=USER1_USERNAME)
+        new_user_first_name = 'New_User_Firstname'
+        user = get_mocked_user(new_user_first_name)
+
+        response = self.c.put(
+           '/auth/users/'+str(u.pk)+'/',
+            data=json.dumps(user),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )
+        res_obj = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(res_obj['first_name'], u.first_name)
+        self.assertEqual(res_obj['first_name'], new_user_first_name)
+    
+    def test_patch_user(self):
+        u = User.objects.get(username=USER1_USERNAME)
+        new_user_first_name = 'New_User_Firstname'
+        user = get_mocked_user(new_user_first_name)
+
+        response = self.c.patch(
+           '/auth/users/'+str(u.pk)+'/',
+            data=json.dumps(user),
+            HTTP_AUTHORIZATION=self.token,
+            content_type=JSON
+        )
+        res_obj = json.loads(response.content.decode('UTF-8'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertNotEqual(res_obj['first_name'], u.first_name)
+        self.assertEqual(res_obj['first_name'], new_user_first_name)
+    
+    def test_delete_user(self):
+        u = User.objects.get(username=USER1_USERNAME)
+        response = self.c.delete('/auth/users/'+str(u.pk)+'/', HTTP_AUTHORIZATION=self.token, content_type=JSON)
+        res_obj = json.loads(response.content.decode('UTF-8'))
+        self.assertEqual(response.status_code, 400)
