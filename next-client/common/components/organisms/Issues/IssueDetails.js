d0f052b283357ad8a6419d731d80618d5dc1824e
Ana Perisic <anaperisictb@yahoo.com>
2022-01-23 16:27:41 +0100

    feature[fe][tab-issues]: added IssueDetails

---
+++ next-client/common/components/organisms/Issues/IssueDetails.js
@@ -0,0 +1,8 @@
+const IssueDetails = ({ issueId }) => {
+  return (
+    <>
+      <h1>Zdravo ja sam issue {issueId}</h1>
+    </>
+  );
+};
+export default IssueDetails;
--- next-client/pages/[user]/[repository]/issues/[idx].js
+++ next-client/pages/[user]/[repository]/issues/[idx].js
@@ -1,10 +1,15 @@
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import IssueDetails from "../../../../common/components/organisms/Issues/IssueDetails";
 
 const IssueIndex = () => {
-	const router = useRouter();
-	const { idx } = router.query;
+  const router = useRouter();
+  const { idx } = router.query;
 
-	return <p>IssueIndex: {idx}</p>;
+  return (
+    <>
+      <IssueDetails issueId={idx}></IssueDetails>
+    </>
+  );
 };
 
 export default IssueIndex;
