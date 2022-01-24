0590449bc438ab02b18b02701ff910e43eff73ab
Ana Perisic <anaperisictb@yahoo.com>
2022-01-24 20:31:42 +0100

    feature[be][tab-issues]: added endpoint for getting issue assgines

--- next-client/common/components/organisms/Issues/IssueDetails.js
+++ next-client/common/components/organisms/Issues/IssueDetails.js
@@ -1,10 +1,10 @@
-import { getIssueById } from "../../../services/progresstrackapp/issuesService";
-import { useState, useEffect } from "react";
-import { Badge, Card } from "react-bootstrap";
-import UserSearch from "../../atoms/UserSearch/UserSearch";
+import { getIssueById } from '../../../services/progresstrackapp/issuesService';
+import { useState, useEffect } from 'react';
+import { Badge, Card } from 'react-bootstrap';
+import UserSearch from '../../atoms/UserSearch/UserSearch';
 
 const IssueDetails = ({ issueId }) => {
-  const [issue, setIssue] = useState("");
+  const [issue, setIssue] = useState('');
 
   useEffect(async () => {
     if (!issueId) return;
@@ -13,40 +13,49 @@ const IssueDetails = ({ issueId }) => {
   }, [issueId]);
   return (
     <>
-      <h3>
-        <div style={{ display: "flex" }}>
-          <Badge pill bg="primary" text="light" style={{ marginRight: "10px" }}>
-            #{issue.pk}
-          </Badge>{" "}
-          {issue.title}
-        </div>
-      </h3>
-      <div>
-        <Card border="light" style={{ width: "25%", marginLeft: "75%" }}>
-          <Card.Header>Assignees</Card.Header>
-          <Card.Body>
-            <UserSearch placeholder="Add an assignee..."></UserSearch>
-          </Card.Body>
-        </Card>
-        <Card
-          border="light"
-          style={{ width: "25%", marginLeft: "75%", marginTop: "25px" }}
-        >
-          <Card.Header>Labels</Card.Header>
-          <Card.Body>
-            <UserSearch placeholder="Add a label..."></UserSearch>
-          </Card.Body>
-        </Card>
-        <Card
-          border="light"
-          style={{ width: "25%", marginLeft: "75%", marginTop: "25px" }}
-        >
-          <Card.Header>Milestones</Card.Header>
-          <Card.Body>
-            <UserSearch placeholder="Add a milestone..."></UserSearch>
-          </Card.Body>
-        </Card>
-      </div>
+      {issue && (
+        <>
+          <h3>
+            <div style={{ display: 'flex' }}>
+              <Badge
+                pill
+                bg="primary"
+                text="light"
+                style={{ marginRight: '10px' }}
+              >
+                #{issue.pk}
+              </Badge>{' '}
+              {issue.title}
+            </div>
+          </h3>
+          <div>
+            <Card border="light" style={{ width: '25%', marginLeft: '75%' }}>
+              <Card.Header>Assignees</Card.Header>
+              <Card.Body>
+                <UserSearch placeholder="Add an assignee..."></UserSearch>
+              </Card.Body>
+            </Card>
+            <Card
+              border="light"
+              style={{ width: '25%', marginLeft: '75%', marginTop: '25px' }}
+            >
+              <Card.Header>Labels</Card.Header>
+              <Card.Body>
+                <UserSearch placeholder="Add a label..."></UserSearch>
+              </Card.Body>
+            </Card>
+            <Card
+              border="light"
+              style={{ width: '25%', marginLeft: '75%', marginTop: '25px' }}
+            >
+              <Card.Header>Milestones</Card.Header>
+              <Card.Body>
+                <UserSearch placeholder="Add a milestone..."></UserSearch>
+              </Card.Body>
+            </Card>
+          </div>
+        </>
+      )}
     </>
   );
 };
