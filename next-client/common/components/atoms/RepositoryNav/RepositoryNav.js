cc4cd05a52c2de11294969343329b685061b6324
Ana Perisic <anaperisictb@yahoo.com>
2022-01-26 00:16:32 +0100

    feature[fe][tab-issues]: added nav for the repo in the milestones & labels

--- next-client/common/components/atoms/RepositoryNav/RepositoryNav.js
+++ next-client/common/components/atoms/RepositoryNav/RepositoryNav.js
@@ -1,28 +1,51 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { getRepositoryById } from '../../../services/versioning/repositoryService';
+import { getUserById } from './../../../services/useractivity/userService';
 
-const RepositoryNav = ({ username, repository }) => {
+const RepositoryNav = () => {
+	const router = useRouter();
+	const { user, repository } = router.query;
+	const [author, setAuthor] = useState();
+	const [authorRepository, setAuthorRepository] = useState();
+
+	useEffect(async () => {
+		if (!user || !repository) return;
+		setAuthorRepository(await getRepositoryById(repository));
+		setAuthor(await getUserById(user));
+	}, [user, repository]);
+
 	return (
-		<div
-			style={{
-				display: 'flex',
-				alignItems: 'baseline',
-				justifyContent: 'space-between',
-			}}
-		>
-			<div>
-				<h4>
-					{/* <Link href={`/${username}`}>
-						<a style={{ textDecoration: 'none', color: '#444' }}>{username}</a>
-					</Link>{' '}
-					/{' '}
-					<Link href={`/${username}/${repository.pk}`}>
-						<a style={{ textDecoration: 'none', color: '#444' }}>
-							{repository.name}
-						</a>
-					</Link>{' '} */}
-				</h4>
-			</div>
-		</div>
+		<>
+			{author && authorRepository && (
+				<div
+					style={{
+						display: 'flex',
+						alignItems: 'baseline',
+						justifyContent: 'space-between',
+						borderBottom: '1px solid #f5f1f1',
+						marginBottom: '10px',
+					}}
+				>
+					<div>
+						<h4>
+							<Link href={`/${author.pk}`}>
+								<a style={{ textDecoration: 'none', color: '#444' }}>
+									{author.username}
+								</a>
+							</Link>{' '}
+							/{' '}
+							<Link href={`/${author.pk}/${authorRepository.pk}`}>
+								<a style={{ textDecoration: 'none', color: '#444' }}>
+									{authorRepository.name}
+								</a>
+							</Link>{' '}
+						</h4>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
