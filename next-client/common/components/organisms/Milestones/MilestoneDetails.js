d0db713452e5607480d6ecbb0b425ede98798d52
Ana Perisic <anaperisictb@yahoo.com>
2022-01-27 19:27:29 +0100

    feature[fe][page-milestones]: search for issues

--- next-client/common/components/organisms/Milestones/MilestoneDetails.js
+++ next-client/common/components/organisms/Milestones/MilestoneDetails.js
@@ -1,16 +1,40 @@
 import RepositoryNav from "../../atoms/RepositoryNav/RepositoryNav";
-import { Badge } from "react-bootstrap";
+import { Badge, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { getMilestoneById } from "../../../services/progresstrackapp/milestonesService";
+import UserSearch from "../../atoms/UserSearch/UserSearch";
+import { useRouter } from "next/router";
+import { getIssueDataForMilestoneIssueSearch } from "../../../services/progresstrackapp/issuesService";
 
 const MilestoneDetails = ({ milestoneId }) => {
   const [milestone, setMilestone] = useState("");
+  const [issueDataForSearch, setIssueDataForSearch] = useState([]);
+  const [milestonesIssue, setMilestonesIssue] = useState([]);
+
+  const router = useRouter();
+  const { repository } = router.query;
 
   useEffect(async () => {
     if (!milestoneId) return;
     let milestone = await getMilestoneById(milestoneId);
     setMilestone(milestone);
   }, [milestoneId]);
+
+  useEffect(async () => {
+    if (!repository) return;
+    //setMilestonesIssue(await getAllIssueLabels(issueId));
+
+    setIssueDataForSearch(
+      await getIssueDataForMilestoneIssueSearch(repository)
+    );
+  }, [repository]);
+
+  const isAlreadyAddedIssue = (issue) => {
+    return milestonesIssue.find(
+      (addedIssue) => addedIssue.title == issue.title
+    );
+  };
+
   return (
     <>
       <RepositoryNav></RepositoryNav>
@@ -22,6 +46,17 @@ const MilestoneDetails = ({ milestoneId }) => {
           {milestone.title}
         </div>
       </h4>
+      <Card style={{ width: "25%", marginLeft: "75%", marginTop: "25px" }}>
+        <Card.Header>Issues</Card.Header>
+        <Card.Body>
+          <UserSearch
+            placeholder="Add an issue..."
+            data={issueDataForSearch.filter(
+              (issue) => !isAlreadyAddedIssue(issue)
+            )}
+          ></UserSearch>
+        </Card.Body>
+      </Card>
     </>
   );
 };
