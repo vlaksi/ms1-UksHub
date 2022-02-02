a6c470537d0481b4566360397c557a3767ccf4af
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-02-03 00:29:00 +0100

    feature[be][git-server]: init endpoint to get branch content

--- django-backend/ukshub/versioningapp/views.py
+++ django-backend/ukshub/versioningapp/views.py
@@ -1,3 +1,8 @@
+import json
+import os
+import time
+from datetime import datetime
+
 from django.db.models import Q
 from ast import Return
 from django.shortcuts import render
@@ -13,11 +18,8 @@ from .serializers import CollaborationTypeSerializer, CollaboratorSerializer, Br
 from .dtos import CollaboratorDto, GitServerBranchDto, GitServerCommitDto
 
 from git import Repo
-import os
-from datetime import datetime
-import time
 
-def get_tree_blobs(tree):
+def get_data_from_tree_blobs(tree, data_obj):
     charset='ascii'
     for blob in tree.blobs:
         print("\n\n")
@@ -27,6 +29,9 @@ def get_tree_blobs(tree):
         print(path)
         print("\ndata.decode(charset)")
         print(data.decode(charset))
+        data_obj[path] = data
+
+    return data_obj
 
 class RepositoryList(generics.ListCreateAPIView):
     search_fields = ['name']
@@ -111,26 +116,41 @@ def repository_branches(request, pk):
     return Response(serializers.data)
 
 @api_view(['GET'])
-def branch_last_commit(request, repository_id, name):
-    repository = Repository.objects.get(id = repository_id)
-    # branch = Branch.objects.get(name = name, repository_id = repository.id)
-    commits = []
+def branch_content(request, repo_id, name):
+    repository = Repository.objects.get(id = repo_id)
+    data = {}
     try:
-        print("\n\n\n\nusli u try branch_last_commita")
+        print("\n\n\n\nusli u branch_content")
         repo = Repo(os.getenv('GIT_SERVER_PATH')+str(repository.author.id)+"/"+repository.name+'.git')
 
         print("\n\tblobs")
         blobs = repo.tree(name).blobs # returns a list of blobs
-        get_tree_blobs(repo.tree(name))
-      
+        data = get_data_from_tree_blobs(repo.tree(name), data)
+        print("data")
             
         # TODO: Make some recursion to get all from trees somehow
 
         trees = repo.tree(name).trees # returns a list of trees
+        print("prosli trees")
         for tree in repo.tree(name).trees:
-            get_tree_blobs(tree)
+            data = get_data_from_tree_blobs(tree, data)
       
         print("\n\nKRAJ\n")
+    except:
+        return Response({"message": "Error during reading repos"})
+
+    return Response(data)
+    
+
+@api_view(['GET'])
+def branch_last_commit(request, repository_id, name):
+    print("\n\nrepository_id")
+    print(repository_id)
+    repository = Repository.objects.get(id = repository_id)
+    # branch = Branch.objects.get(name = name, repository_id = repository.id)
+    commits = []
+    try:
+        repo = Repo(os.getenv('GIT_SERVER_PATH')+str(repository.author.id)+"/"+repository.name+'.git')
         commits = list(repo.iter_commits(name))
     except:
         return Response({})
