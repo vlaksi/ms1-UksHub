aa605b6dd8a3100cd8714a61f9b402e726c130e6
Ana Perisic <anaperisictb@yahoo.com>
2022-02-02 11:26:37 +0100

    feature[fe][reactions]: finished heart reaction

--- next-client/common/components/atoms/Comments/CommentListItem.js
+++ next-client/common/components/atoms/Comments/CommentListItem.js
@@ -1,23 +1,23 @@
-import { Card, Badge, Modal, Button, Form } from 'react-bootstrap';
-import { MdEdit } from 'react-icons/md';
-import { AiFillDelete, AiFillLike, AiFillDislike } from 'react-icons/ai';
-import { BsFillChatQuoteFill } from 'react-icons/bs';
-import { FcLike } from 'react-icons/fc';
+import { Card, Badge, Modal, Button, Form } from "react-bootstrap";
+import { MdEdit } from "react-icons/md";
+import { AiFillDelete, AiFillLike } from "react-icons/ai";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { RiHeart3Fill } from "react-icons/ri";
 
 import {
   deleteComment,
   updateComment,
-} from '../../../services/useractivity/commentsService';
-import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import { getUserById } from '../../../services/useractivity/userService';
+} from "../../../services/useractivity/commentsService";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { getUserById } from "../../../services/useractivity/userService";
 import {
   addReaction,
   deleteReactionByCommentAndUserId,
   getAllReactionsByCommentId,
-} from '../../../services/useractivity/reactionsService';
-import { getParsedToken } from '../../../services/authentication/token';
+} from "../../../services/useractivity/reactionsService";
+import { getParsedToken } from "../../../services/authentication/token";
 
 const CommentListItem = ({
   comment,
@@ -36,6 +36,7 @@ const CommentListItem = ({
   };
 
   const [isCommentLikedByMe, setIsCommentLikedByMe] = useState(true);
+  const [isCommentHeartByMe, setIsCommentHeartByMe] = useState(true);
 
   const [commentMessage, setCommentMessage] = useState(comment.message);
   const handleChangingCommentMessage = (commentMessage) => {
@@ -44,12 +45,13 @@ const CommentListItem = ({
   const [reactions, setReactions] = useState([]);
 
   const [showNumberOfLikes, setShowNumberOfLikes] = useState([]);
+  const [showNumberOfHearts, setShowNumberOfHearts] = useState([]);
 
-  const notifyDeleted = () => toast.success('Successfully deleted comment!');
+  const notifyDeleted = () => toast.success("Successfully deleted comment!");
 
   const notifyUpdated = () =>
-    toast.success('Successfully changed comment message!');
-  const notifyError = () => toast.error('Check if you entered all fields!');
+    toast.success("Successfully changed comment message!");
+  const notifyError = () => toast.error("Check if you entered all fields!");
 
   const router = useRouter();
   const { user, repository } = router.query;
@@ -84,7 +86,9 @@ const CommentListItem = ({
   useEffect(async () => {
     if (!comment?.pk) return;
     let newReactions = await setNumberOfLikes();
+    let newReactionsHeart = await setNumberOfHearts();
     checkIsCommentLikedByMe(newReactions);
+    checkIsCommentHeartByMe(newReactionsHeart);
   }, [comment?.pk]);
 
   const setNumberOfLikes = async () => {
@@ -95,17 +99,28 @@ const CommentListItem = ({
     }
 
     setReactions(newReactions);
-    newReactions = newReactions.filter((reaction) => reaction.type == 'like');
+    newReactions = newReactions.filter((reaction) => reaction.type == "like");
 
     let numberOfLikes = newReactions?.length;
-    console.log('Svi brojevi lajkova su:', numberOfLikes);
     setShowNumberOfLikes(numberOfLikes);
+    return newReactions;
+  };
+  const setNumberOfHearts = async () => {
+    let newReactions = await getAllReactionsByCommentId(comment.pk);
+    if (!newReactions) {
+      setShowNumberOfHearts(0);
+      return;
+    }
+
+    setReactions(newReactions);
+    newReactions = newReactions.filter((reaction) => reaction.type == "heart");
+
+    let numberOfHearts = newReactions?.length;
+    setShowNumberOfHearts(numberOfHearts);
     return newReactions;
   };
 
   const checkIsCommentLikedByMe = (reactions) => {
-    console.log('\ncheckIsCommentLikedByMe');
-    console.log('newReactions: ', reactions);
     let foundReaction = reactions?.find(
       (reaction) => reaction.author == getLoggedInUserId()
     );
@@ -113,6 +128,16 @@ const CommentListItem = ({
       setIsCommentLikedByMe(true);
     } else {
       setIsCommentLikedByMe(false);
+    }
+  };
+  const checkIsCommentHeartByMe = (reactions) => {
+    let foundReaction = reactions?.find(
+      (reaction) => reaction.author == getLoggedInUserId()
+    );
+    if (foundReaction) {
+      setIsCommentHeartByMe(true);
+    } else {
+      setIsCommentHeartByMe(false);
     }
   };
 
@@ -132,13 +157,13 @@ const CommentListItem = ({
   return (
     <>
       <div>
-        <Card style={{ marginTop: '5%' }}>
+        <Card style={{ marginTop: "5%" }}>
           <Card.Header>
-            Commented by{' '}
+            Commented by{" "}
             <Badge bg="primary" text="light" pill>
               {author?.username}
-            </Badge>{' '}
-            on{' '}
+            </Badge>{" "}
+            on{" "}
             <Badge bg="primary" text="light" pill>
               {comment.creation_date.substring(0, 10)}
             </Badge>
@@ -147,15 +172,15 @@ const CommentListItem = ({
             <Card.Text>{comment.message}</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
               {/* Reactions */}
-              <div style={{ marginLeft: '10px', display: 'flex' }}>
+              <div style={{ marginLeft: "10px", display: "flex" }}>
                 {/* Like */}
                 <div
                   style={{
-                    display: 'flex ',
-                    marginRight: '10px',
-                    alignItems: 'center',
+                    display: "flex ",
+                    marginRight: "10px",
+                    alignItems: "center",
                   }}
                 >
                   {isCommentLikedByMe ? (
@@ -167,15 +192,15 @@ const CommentListItem = ({
                         await deleteReactionByCommentAndUserId(
                           comment.pk,
                           loggedInUser,
-                          'like'
+                          "like"
                         );
                         setNumberOfLikes();
                         setIsCommentLikedByMe(false);
                       }}
                       style={{
-                        color: 'blue',
-                        cursor: 'pointer',
-                        marginRight: '5px',
+                        color: "blue",
+                        cursor: "pointer",
+                        marginRight: "5px",
                       }}
                     />
                   ) : (
@@ -183,15 +208,14 @@ const CommentListItem = ({
                     <AiFillLike
                       size={18}
                       onClick={async () => {
-                        console.log('click on like');
                         let loggedInUser = getLoggedInUserId();
-                        await addReaction(loggedInUser, 'like', comment.pk);
+                        await addReaction(loggedInUser, "like", comment.pk);
                         setNumberOfLikes();
                         setIsCommentLikedByMe(true);
                       }}
                       style={{
-                        cursor: 'pointer',
-                        marginRight: '5px',
+                        cursor: "pointer",
+                        marginRight: "5px",
                       }}
                     />
                   )}
@@ -202,23 +226,48 @@ const CommentListItem = ({
                 {/* Heart */}
                 <div
                   style={{
-                    display: 'flex ',
-                    marginLeft: '5px',
-                    alignItems: 'center',
+                    display: "flex ",
+                    marginLeft: "5px",
+                    alignItems: "center",
                   }}
                 >
-                  <FcLike
-                    size={20}
-                    onClick={() => {
-                      console.log('click on heart');
-                    }}
-                    style={{
-                      cursor: 'pointer',
-                      color: 'red',
-                      marginRight: '5px',
-                    }}
-                  />
-                  99
+                  {isCommentHeartByMe ? (
+                    <RiHeart3Fill
+                      color="black"
+                      size={20}
+                      onClick={async () => {
+                        let loggedInUser = getLoggedInUserId();
+                        await deleteReactionByCommentAndUserId(
+                          comment.pk,
+                          loggedInUser,
+                          "heart"
+                        );
+                        setNumberOfHearts();
+                        setIsCommentHeartByMe(false);
+                      }}
+                      style={{
+                        cursor: "pointer",
+                        color: "red",
+                        marginRight: "5px",
+                      }}
+                    />
+                  ) : (
+                    <RiHeart3Fill
+                      size={20}
+                      onClick={async () => {
+                        let loggedInUser = getLoggedInUserId();
+                        await addReaction(loggedInUser, "heart", comment.pk);
+                        setNumberOfHearts();
+                        setIsCommentHeartByMe(true);
+                      }}
+                      style={{
+                        cursor: "pointer",
+                        color: "black",
+                        marginRight: "5px",
+                      }}
+                    />
+                  )}
+                  {showNumberOfHearts}
                 </div>
               </div>
 
@@ -228,8 +277,8 @@ const CommentListItem = ({
                   <MdEdit
                     size={20}
                     style={{
-                      cursor: 'pointer',
-                      color: 'green',
+                      cursor: "pointer",
+                      color: "green",
                     }}
                     onClick={() => {
                       handleShow();
@@ -241,10 +290,10 @@ const CommentListItem = ({
                   <AiFillDelete
                     size={18}
                     style={{
-                      cursor: 'pointer',
-                      color: 'red',
-                      marginLeft: '10px',
-                      marginRight: '10px',
+                      cursor: "pointer",
+                      color: "red",
+                      marginLeft: "10px",
+                      marginRight: "10px",
                     }}
                     onClick={handleShowDeleteModal}
                   />
@@ -252,7 +301,7 @@ const CommentListItem = ({
 
                 <BsFillChatQuoteFill
                   size={15}
-                  style={{ cursor: 'pointer' }}
+                  style={{ cursor: "pointer" }}
                   onClick={() => {
                     setNewCommentMessage('" ' + comment.message + ' "');
                     handleShowAddComment();
@@ -268,9 +317,9 @@ const CommentListItem = ({
           </Modal.Header>
           <Modal.Body
             style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: ' baseline',
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: " baseline",
             }}
           >
             <p>Are you sure you want to remove chosen comment?</p>
--- next-client/common/services/useractivity/reactionsService.js
+++ next-client/common/services/useractivity/reactionsService.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import { getToken } from '../authentication/token';
+import axios from "axios";
+import { getToken } from "../authentication/token";
 
 export const getAllReactionsByCommentId = async (commentId) => {
   let reactions;
   await axios
     .request({
       url: `/useractivity/reactions/comment/${commentId}`,
-      method: 'get',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
+      method: "get",
+      baseURL: "http://127.0.0.1:8000/",
+      headers: { Authorization: "JWT " + getToken() },
       data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
+        grant_type: "client_credentials",
+        scope: "public",
       },
     })
     .then((response) => {
@@ -28,15 +28,15 @@ export const addReaction = async (authorId, reactionType, commentId) => {
   await axios
     .request({
       url: `/useractivity/reactions/`,
-      method: 'post',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
+      method: "post",
+      baseURL: "http://127.0.0.1:8000/",
+      headers: { Authorization: "JWT " + getToken() },
       data: {
         author: authorId,
         type: reactionType,
         comment: commentId,
-        grant_type: 'client_credentials',
-        scope: 'public',
+        grant_type: "client_credentials",
+        scope: "public",
       },
     })
     .then((response) => {
@@ -58,12 +58,12 @@ export const deleteReactionByCommentAndUserId = async (
   await axios
     .request({
       url: `/useractivity/reactions/comments/${commentId}/user/${userId}/type/${reactionType}`,
-      method: 'delete',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
+      method: "delete",
+      baseURL: "http://127.0.0.1:8000/",
+      headers: { Authorization: "JWT " + getToken() },
       data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
+        grant_type: "client_credentials",
+        scope: "public",
       },
     })
     .then((response) => {
