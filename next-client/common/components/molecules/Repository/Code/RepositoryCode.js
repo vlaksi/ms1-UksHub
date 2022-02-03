aacdc3014539ad0b8f2c358f481203f1384647be
Vladislav Maksimovic <daksrapsody@gmail.com>
2022-02-03 01:28:25 +0100

    feature[fe][git-server]: show files

--- next-client/common/components/molecules/Repository/Code/RepositoryCode.js
+++ next-client/common/components/molecules/Repository/Code/RepositoryCode.js
@@ -1,255 +1,281 @@
 import { useEffect, useState } from 'react';
-import { Button, Modal, Form, Card, Dropdown, ListGroup } from 'react-bootstrap';
+import {
+	Button,
+	Modal,
+	Form,
+	Card,
+	Dropdown,
+	ListGroup,
+} from 'react-bootstrap';
 import { BsFillFolderFill } from 'react-icons/bs';
 import { MdAddCircle } from 'react-icons/md';
 import { AiFillHome, AiOutlineFile } from 'react-icons/ai';
 import { ToastContainer, toast } from 'react-toastify';
 import styles from './RepositoryCode.module.scss';
 
-import { getBranchCommits, getBranchLastCommit } from '../../../../services/versioning/repositoryService';
-import { createBranch, deleteBranch, getRepositoryBranches } from '../../../../services/versioning/branchService';
+import {
+	getBranchCommits,
+	getBranchLastCommit,
+} from '../../../../services/versioning/repositoryService';
+import {
+	createBranch,
+	deleteBranch,
+	getBranchContent,
+	getRepositoryBranches,
+} from '../../../../services/versioning/branchService';
 
-const RepositoryCode = ({ repository, repositoryBranches, isLoggedInUserCollaborator }) => {
-  const [show, setShow] = useState(false);
-  const [branches, setBranches] = useState(repositoryBranches);
-  const [newBranchName, setNewBranchName] = useState('');
-  const [deleteBranchId, setDeleteBranch] = useState('');
-  const [activeBranch, setActiveBranch] = useState(repositoryBranches[0]);
-  const [activeFolders, setActiveFolders] = useState([]);
-  const [activeFiles, setActiveFiles] = useState([]);
-  const [activeFilesPath, setActiveFilesPath] = useState([]);
-  const [commit, setCommit] = useState([]);
-  const [commits, setCommits] = useState([]);
+const RepositoryCode = ({
+	repository,
+	repositoryBranches,
+	isLoggedInUserCollaborator,
+}) => {
+	const [show, setShow] = useState(false);
+	const [branches, setBranches] = useState(repositoryBranches);
+	const [newBranchName, setNewBranchName] = useState('');
+	const [deleteBranchId, setDeleteBranch] = useState('');
+	const [activeBranch, setActiveBranch] = useState(repositoryBranches[0]);
+	const [activeFolders, setActiveFolders] = useState([]);
+	const [activeFiles, setActiveFiles] = useState([]);
+	const [activeFilesPath, setActiveFilesPath] = useState([]);
+	const [commit, setCommit] = useState([]);
+	const [commits, setCommits] = useState([]);
 
-  const setCurrentBrach = async (branch) => {
-    setActiveBranch(branch);
-    var commit = await getBranchLastCommit(repository.pk, branch.name);
-    setCommit(commit);
-    var commits = await getBranchCommits(repository.pk, branch.name);
-    setCommits(commits);
-    console.log(commits);
-  };
+	const setCurrentBrach = async (branch) => {
+		setActiveBranch(branch);
+		var commit = await getBranchLastCommit(repository.pk, branch.name);
+		setCommit(commit);
+		var commits = await getBranchCommits(repository.pk, branch.name);
+		setCommits(commits);
+		console.log(commits);
+	};
 
-  const notify = () => toast.success('Successfully created new branch!');
-  const notifyDeletedBranch = () => toast.success('Successfully deleted branch!');
-  const notifyError = () => toast.error('Check if you entered all fields!');
+	const notify = () => toast.success('Successfully created new branch!');
+	const notifyDeletedBranch = () =>
+		toast.success('Successfully deleted branch!');
+	const notifyError = () => toast.error('Check if you entered all fields!');
 
-  const showAddBranch = () => {
-    setShow(true);
-  };
+	const showAddBranch = () => {
+		setShow(true);
+	};
 
-  const handleClose = () => {
-    setShow(false);
-    setNewBranchName('');
-  };
+	const handleClose = () => {
+		setShow(false);
+		setNewBranchName('');
+	};
 
-  const handleBranchNameChange = (newBranchName) => {
-    setNewBranchName(newBranchName);
-  };
+	const handleBranchNameChange = (newBranchName) => {
+		setNewBranchName(newBranchName);
+	};
 
-  const handleDeleteBranchChange = (deleteBranchId) => {
-    setDeleteBranch(deleteBranchId);
-  };
+	const handleDeleteBranchChange = (deleteBranchId) => {
+		setDeleteBranch(deleteBranchId);
+	};
 
-  const addBranch = async () => {
-    let createdBranch = await createBranch(repository.pk, newBranchName);
+	const addBranch = async () => {
+		let createdBranch = await createBranch(repository.pk, newBranchName);
 
-    if (!createdBranch) {
-      notifyError();
-      return;
-    } else {
-      notify();
-      handleClose();
-      setBranches(await getRepositoryBranches(repository.pk));
-    }
-  };
+		if (!createdBranch) {
+			notifyError();
+			return;
+		} else {
+			notify();
+			handleClose();
+			setBranches(await getRepositoryBranches(repository.pk));
+		}
+	};
 
-  const removeBranch = async () => {
-    await deleteBranch(deleteBranchId);
-    notifyDeletedBranch();
-    setBranches(await getRepositoryBranches(repository.pk));
-  };
+	const removeBranch = async () => {
+		await deleteBranch(deleteBranchId);
+		notifyDeletedBranch();
+		setBranches(await getRepositoryBranches(repository.pk));
+	};
 
-  useEffect(async () => {
-    // repositoryBranches = await getRepositoryBranches(repository.pk);
-    // console.log(repositoryBranches);
-    setCurrentBrach(repositoryBranches[0]);
-  }, []);
+	useEffect(async () => {
+		// repositoryBranches = await getRepositoryBranches(repository.pk);
+		// console.log(repositoryBranches);
+		setCurrentBrach(repositoryBranches[0]);
+		let branchContent = await getBranchContent(
+			repository.pk,
+			activeBranch?.name
+		);
+		console.log('branchContent: ', branchContent);
+		setActiveFiles(branchContent);
+	}, []);
 
-  return (
-    <>
-      <Modal show={show} onHide={handleClose} backdrop="static">
-        <Modal.Header closeButton>
-          <Modal.Title>Manage branches</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div>
-            <h5>Add new branch</h5>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Control
-                  type="name"
-                  placeholder="Enter branch name"
-                  value={newBranchName}
-                  onChange={(e) => {
-                    handleBranchNameChange(e.target.value);
-                  }}
-                ></Form.Control>
-              </Form.Group>
-            </Form>
-            <div align="right">
-              <Button
-                variant="success"
-                onClick={() => {
-                  addBranch();
-                }}
-              >
-                Add Branch
-              </Button>
-            </div>
-          </div>
-          <div>
-            <h5>Delete branch</h5>
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Select
-                  aria-label="Default select example"
-                  onChange={(e) => {
-                    handleDeleteBranchChange(e.target.value);
-                  }}
-                >
-                  {branches?.map((branch) => {
-                    return (
-                      <option key={branch?.pk} value={branch?.pk}>
-                        {branch?.name}
-                      </option>
-                    );
-                  })}
-                </Form.Select>
-              </Form.Group>
-            </Form>
-            <div align="right">
-              <Button
-                variant="warning"
-                onClick={() => {
-                  removeBranch();
-                }}
-              >
-                Delete Branch
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handleClose}>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Card>
-        <Card.Header>
-          <div className={styles.repositoryHeader}>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'baseline',
-              }}
-            >
-              <Dropdown>
-                <Dropdown.Toggle variant="dark" id="dropdown-basic">
-                  {activeBranch?.name}
-                </Dropdown.Toggle>
+	return (
+		<>
+			<Modal show={show} onHide={handleClose} backdrop="static">
+				<Modal.Header closeButton>
+					<Modal.Title>Manage branches</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div>
+						<h5>Add new branch</h5>
+						<Form>
+							<Form.Group className="mb-3">
+								<Form.Control
+									type="name"
+									placeholder="Enter branch name"
+									value={newBranchName}
+									onChange={(e) => {
+										handleBranchNameChange(e.target.value);
+									}}
+								></Form.Control>
+							</Form.Group>
+						</Form>
+						<div align="right">
+							<Button
+								variant="success"
+								onClick={() => {
+									addBranch();
+								}}
+							>
+								Add Branch
+							</Button>
+						</div>
+					</div>
+					<div>
+						<h5>Delete branch</h5>
+						<Form>
+							<Form.Group className="mb-3">
+								<Form.Select
+									aria-label="Default select example"
+									onChange={(e) => {
+										handleDeleteBranchChange(e.target.value);
+									}}
+								>
+									{branches?.map((branch) => {
+										return (
+											<option key={branch?.pk} value={branch?.pk}>
+												{branch?.name}
+											</option>
+										);
+									})}
+								</Form.Select>
+							</Form.Group>
+						</Form>
+						<div align="right">
+							<Button
+								variant="warning"
+								onClick={() => {
+									removeBranch();
+								}}
+							>
+								Delete Branch
+							</Button>
+						</div>
+					</div>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="danger" onClick={handleClose}>
+						Cancel
+					</Button>
+				</Modal.Footer>
+			</Modal>
+			<Card>
+				<Card.Header>
+					<div className={styles.repositoryHeader}>
+						<div
+							style={{
+								display: 'flex',
+								alignItems: 'baseline',
+							}}
+						>
+							<Dropdown>
+								<Dropdown.Toggle variant="dark" id="dropdown-basic">
+									{activeBranch?.name}
+								</Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  {branches?.map((branch) => {
-                    return (
-                      <Dropdown.Item
-                        key={branch.name}
-                        onClick={() => {
-                          setCurrentBrach(branch);
-                        }}
-                      >
-                        {branch.name}
-                      </Dropdown.Item>
-                    );
-                  })}
-                </Dropdown.Menu>
-              </Dropdown>
-              <div className={styles.filesPath}>
-                {activeFilesPath.map((activePath, idx) => {
-                  return (
-                    <p key={activePath}>
-                      {idx === 0 ? '' : '/'} {activePath}
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
-            {isLoggedInUserCollaborator && (
-              <div>
-                <Button variant="outline-primary" onClick={showAddBranch}>
-                  {' '}
-                  <MdAddCircle size={24} /> Manage branches
-                </Button>
-              </div>
-            )}
+								<Dropdown.Menu>
+									{branches?.map((branch) => {
+										return (
+											<Dropdown.Item
+												key={branch.name}
+												onClick={() => {
+													setCurrentBrach(branch);
+												}}
+											>
+												{branch.name}
+											</Dropdown.Item>
+										);
+									})}
+								</Dropdown.Menu>
+							</Dropdown>
+							<div className={styles.filesPath}>
+								{activeFilesPath.map((activePath, idx) => {
+									return (
+										<p key={activePath}>
+											{idx === 0 ? '' : '/'} {activePath}
+										</p>
+									);
+								})}
+							</div>
+						</div>
+						{isLoggedInUserCollaborator && (
+							<div>
+								<Button variant="outline-primary" onClick={showAddBranch}>
+									{' '}
+									<MdAddCircle size={24} /> Manage branches
+								</Button>
+							</div>
+						)}
 
-            <div>
-              <AiFillHome
-                onClick={() => {
-                  setActiveFolders(activeBranch?.folders);
-                  setActiveFiles(activeBranch?.files);
-                  setActiveFilesPath([]);
-                }}
-                style={{
-                  cursor: 'pointer',
-                  marginRight: '10px',
-                  height: '20px',
-                  width: '20px',
-                }}
-              />
-            </div>
-          </div>
-        </Card.Header>
-        <Card.Body>
-          <ListGroup>
-            {activeFolders?.map((folder) => {
-              return (
-                <ListGroup.Item key={folder.name} onClick={() => {}}>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <BsFillFolderFill style={{ marginRight: '8px' }} />
-                    {folder.name}
-                  </div>
-                </ListGroup.Item>
-              );
-            })}
-            {activeFiles?.map((file) => {
-              return (
-                <ListGroup.Item
-                  action
-                  key={file.name}
-                  onClick={() => {
-                    alert(file.name);
-                  }}
-                >
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <AiOutlineFile style={{ marginRight: '8px' }} /> {file.name}
-                  </div>
-                </ListGroup.Item>
-              );
-            })}
-          </ListGroup>
-        </Card.Body>
-        <Card.Footer className="text-muted">
-          <div className={styles.repositoryFooter}>
-            {commit != null && commit[0] != null && commit[0].committed_date}
-            &nbsp; &nbsp; &nbsp; &nbsp;
-            <span> {commits != null && commits.length} commits</span>
-          </div>
-        </Card.Footer>
-      </Card>
-    </>
-  );
+						<div>
+							<AiFillHome
+								onClick={() => {
+									setActiveFolders(activeBranch?.folders);
+									setActiveFiles(activeBranch?.files);
+									setActiveFilesPath([]);
+								}}
+								style={{
+									cursor: 'pointer',
+									marginRight: '10px',
+									height: '20px',
+									width: '20px',
+								}}
+							/>
+						</div>
+					</div>
+				</Card.Header>
+				<Card.Body>
+					<ListGroup>
+						{activeFolders?.map((folder) => {
+							return (
+								<ListGroup.Item key={folder.name} onClick={() => {}}>
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<BsFillFolderFill style={{ marginRight: '8px' }} />
+										{folder.name}
+									</div>
+								</ListGroup.Item>
+							);
+						})}
+						{activeFiles?.map((file) => {
+							return (
+								<ListGroup.Item
+									action
+									key={file.name}
+									onClick={() => {
+										alert(file.name);
+									}}
+								>
+									<div style={{ display: 'flex', alignItems: 'center' }}>
+										<AiOutlineFile style={{ marginRight: '8px' }} /> {file.name}
+									</div>
+								</ListGroup.Item>
+							);
+						})}
+					</ListGroup>
+				</Card.Body>
+				<Card.Footer className="text-muted">
+					<div className={styles.repositoryFooter}>
+						{commit != null && commit[0] != null && commit[0].committed_date}
+						&nbsp; &nbsp; &nbsp; &nbsp;
+						<span> {commits != null && commits.length} commits</span>
+					</div>
+				</Card.Footer>
+			</Card>
+		</>
+	);
 };
 
 export default RepositoryCode;
--- next-client/common/services/versioning/branchService.js
+++ next-client/common/services/versioning/branchService.js
@@ -2,133 +2,155 @@ import axios from 'axios';
 import { getToken } from '../authentication/token';
 
 export const getDefaultBranch = async (branchId) => {
-  let branch;
-  await axios
-    .request({
-      url: `/versioning/branchs/${branchId}`,
-      method: 'get',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      branch = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branch;
+	let branch;
+	await axios
+		.request({
+			url: `/versioning/branchs/${branchId}`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branch = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branch;
+};
+
+export const getBranchContent = async (repositoryId, branchName) => {
+	let content;
+	await axios
+		.request({
+			url: `/versioning/repository/${repositoryId}/branch/${branchName}/content`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			content = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return content;
 };
 
 export const getCommitsBranch = async (branchId) => {
-  let branch;
-  await axios
-    .request({
-      url: `/versioning/branchs/${branchId}`,
-      method: 'get',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      branch = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branch;
+	let branch;
+	await axios
+		.request({
+			url: `/versioning/branchs/${branchId}`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branch = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branch;
 };
 
 export const getRepositoryBranches = async (repositoryId) => {
-  let branches;
-  await axios
-    .request({
-      url: `/versioning/repository/${repositoryId}/branches/`,
-      method: 'get',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      branches = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branches;
+	let branches;
+	await axios
+		.request({
+			url: `/versioning/repository/${repositoryId}/branches/`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branches = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branches;
 };
 export const getMainBranchCommits = async (repoId) => {
-  let commits;
-  await axios
-    .request({
-      url: `/versioning/mainbranch/${repoId}/commits/`,
-      method: 'get',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      commits = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return commits;
+	let commits;
+	await axios
+		.request({
+			url: `/versioning/mainbranch/${repoId}/commits/`,
+			method: 'get',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			commits = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return commits;
 };
 export const createBranch = async (repositoryId, branchName = 'main') => {
-  let branch = null;
-  await axios
-    .request({
-      url: `/versioning/branchs/`,
-      method: 'post',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        name: branchName,
-        repository: repositoryId,
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      branch = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branch;
+	let branch = null;
+	await axios
+		.request({
+			url: `/versioning/branchs/`,
+			method: 'post',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				name: branchName,
+				repository: repositoryId,
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branch = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branch;
 };
 
 export const deleteBranch = async (branchId) => {
-  let branch = null;
-  await axios
-    .request({
-      url: `/versioning/branchs/${branchId}`,
-      method: 'delete',
-      baseURL: 'http://127.0.0.1:8000/',
-      headers: { Authorization: 'JWT ' + getToken() },
-      data: {
-        grant_type: 'client_credentials',
-        scope: 'public',
-      },
-    })
-    .then((response) => {
-      branch = response.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  return branch;
+	let branch = null;
+	await axios
+		.request({
+			url: `/versioning/branchs/${branchId}`,
+			method: 'delete',
+			baseURL: 'http://127.0.0.1:8000/',
+			headers: { Authorization: 'JWT ' + getToken() },
+			data: {
+				grant_type: 'client_credentials',
+				scope: 'public',
+			},
+		})
+		.then((response) => {
+			branch = response.data;
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+	return branch;
 };
